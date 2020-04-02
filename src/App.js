828e1910d88be54467ce32a87b3a867ce03b5ba4
Anna Shpak <anna.shpak@nordicsoft.team>
2020-04-02 09:50:44 +0300

    Test commit with functional component and new default user

--- src/App.js
+++ src/App.js
@@ -21,90 +21,89 @@ import "./styles.css";
 
 const API = "https://jsonplaceholder.typicode.com/users";
 
-class UserDetail extends React.Component {
-  render() {
-    if (this.props.user === undefined) {
-      return <div />;
-    } else {
-      const user = this.props.user;
-      return (
-        <div>
-          <h4 className="mt-5">Information about active user</h4>
-          <div className="detail-block">
-            <div>
-              <span>Name: </span>
-              {user.name}
-            </div>
-            <div>
-              <span>Username: </span>
-              {user.username}
-            </div>
-            <div>
-              <span>E-mail: </span>
-              {user.email}
-            </div>
-            <div>
-              <span>Address: </span>
-              <div className="tab-list">
-                <div>
-                  <span>Street: </span>
-                  {user.address.street}
-                </div>
-                <div>
-                  <span>Suite: </span>
-                  {user.address.suite}
-                </div>
-                <div>
-                  <span>City: </span>
-                  {user.address.city}
-                </div>
-                <div>
-                  <span>Zip Code: </span>
-                  {user.address.zipcode}
-                </div>
-                <div>
-                  <span>Geo: </span>
-                  <div className="tab-list">
-                    <div>
-                      <span>Lat: </span>
-                      {user.address.geo.lat}
-                    </div>
-                    <div>
-                      <span>Lng: </span>
-                      {user.address.geo.lng}
-                    </div>
+function UserDetail({user}){
+  
+  if (user === undefined) {
+    return <div />;
+  } else {
+    return (
+      <div>
+        <h4 className="mt-5">Information about active user</h4>
+        <div className="detail-block">
+          <div>
+            <span>Name: </span>
+            {user.name}
+          </div>
+          <div>
+            <span>Username: </span>
+            {user.username}
+          </div>
+          <div>
+            <span>E-mail: </span>
+            {user.email}
+          </div>
+          <div>
+            <span>Address: </span>
+            <div className="tab-list">
+              <div>
+                <span>Street: </span>
+                {user.address.street}
+              </div>
+              <div>
+                <span>Suite: </span>
+                {user.address.suite}
+              </div>
+              <div>
+                <span>City: </span>
+                {user.address.city}
+              </div>
+              <div>
+                <span>Zip Code: </span>
+                {user.address.zipcode}
+              </div>
+              <div>
+                <span>Geo: </span>
+                <div className="tab-list">
+                  <div>
+                    <span>Lat: </span>
+                    {user.address.geo.lat}
+                  </div>
+                  <div>
+                    <span>Lng: </span>
+                    {user.address.geo.lng}
                   </div>
                 </div>
               </div>
             </div>
-            <div>
-              <span>Phone: </span>
-              {user.phone}
-            </div>
-            <div>
-              <span>Website: </span>
-              {user.website}
-            </div>
-            <div>
-              <span>Company: </span>
-              <div className="tab-list">
-                <div>
-                  <span>Name: </span>
-                  {user.company.name}
-                </div>
-                <div>
-                  <span>Catch Phrase: </span> {user.company.catchPhrase}
-                </div>
-                <div>
-                  <span>Bs: </span> {user.company.bs}
-                </div>
+          </div>
+          <div>
+            <span>Phone: </span>
+            {user.phone}
+          </div>
+          <div>
+            <span>Website: </span>
+            {user.website}
+          </div>
+          <div>
+            <span>Company: </span>
+            <div className="tab-list">
+              <div>
+                <span>Name: </span>
+                {user.company.name}
+              </div>
+              <div>
+                <span>Catch Phrase: </span> {user.company.catchPhrase}
+              </div>
+              <div>
+                <span>Bs: </span> {user.company.bs}
               </div>
             </div>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
+
 }
 
 class UserRow extends React.Component {
